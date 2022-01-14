fdb46fb3bfe9b036a6c9fe1110d62ebabcfd4aa3
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-01-14 20:40:08 +0530

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'prettier/prettier': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.{js,jsx}', '**/*.test.{js,jsx}', '**/*.spec.{js,jsx}'],
+      },
+    ],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/no-danger': 'off',
+    'react/jsx-props-no-spreading': ['error', { exceptions: ['button'] }],
+  },
+};
