fe47ea62f8c670c08f1a14af8948ac1a80fcb971
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-01-16 18:03:47 +0530

    Added formatOptions and helper-funcs files

---
+++ src/utils/formatOptions.js
@@ -0,0 +1,17 @@
+export const currencyFormat = {
+  style: 'currency',
+  currency: 'USD',
+  currencyDisplay: 'narrowSymbol',
+  useGrouping: false,
+};
+
+export const percentFormat = {
+  useGrouping: false,
+  style: 'percent',
+  minimumFractionDigits: 2,
+};
+
+export const compactNumberFormat = {
+  notation: 'compact',
+  compactDisplay: 'short',
+};
---
+++ src/utils/helper-funcs.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-param-reassign */
+export const getFirstChar = (str = '') => str.charAt(0);
+
+export const toStartCase = (str = '') => {
+  if (str.includes('_')) {
+    const arr = str.split('_');
+    const modifiedStr = arr.map((item) => {
+      let mStr = item.charAt(0).toUpperCase();
+      mStr += item.slice(1);
+      return mStr;
+    });
+    return modifiedStr.join(' ');
+  }
+
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const formatDate = (timeStamp = '') => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const date = new Date(timeStamp);
+  return date.toLocaleDateString('en-IN', options);
+};
+
+export const formatNumber = (
+  data = '',
+  formatOptions = { maximumSignificantDigits: 3, useGrouping: true },
+  locale = 'en-IN'
+) => {
+  if (typeof data !== 'number') {
+    return data;
+  }
+
+  const value = new Intl.NumberFormat(locale, formatOptions);
+
+  return value.format(data);
+};
+export const getPercentage = (value1, value2) => {
+  const percent = value1 / value2;
+  return percent;
+};
+
+export const formatRevenue = (revenue) => revenue.toFixed(2);
+
+export const getTotal = (dataArr = [], key = '') => {
+  return dataArr.reduce((acc, current) => {
+    acc += current[key];
+    return acc;
+  }, 0);
+};
